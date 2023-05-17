8c6874d0f40ae56854b734c618e2a72353756fbe
Anushil Timsina <ianushil001@gmail.com>
2023-05-17 23:59:44 +0545

    Af

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask,request,render_template
 from database import  insert_user
 from risk_calc import calculate_recommendations
+import yfinance as yf
+from flask import jsonify
+
 
 # define the app using Flask
 app = Flask(__name__)
@@ -33,6 +36,26 @@ def process_form():
     recommendations = calculate_recommendations(age, income, experience, horizon, risk, style, products, updates)
 
     return render_template('results.html', recommendations=recommendations)
+
+@app.route('/get_company_data')
+def get_company_data():
+    symbol = request.args.get('symbol')
+    company = yf.Ticker(symbol)
+    history = company.history(period='max')
+    data = {
+        'dates': list(history.index.strftime('%Y-%m-%d')),
+        'prices': list(history['Close'])
+    }
+    return jsonify(data)
+
+@app.route('/update_notification_preferences', methods=['POST'])
+def update_notification_preferences():
+    email = request.form.get('email')
+    threshold = request.form.get('threshold')
+    symbol = request.form.get('symbol')
+    # Save notification preferences to database or file system
+    return 'Notification preferences updated successfully.'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
